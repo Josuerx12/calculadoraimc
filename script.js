4bdfdc44e15eb48b98fc9c21ace4f24bf95422da
josuerx12 <josueaze12@gmail.com>
2023-04-05 13:15:03 -0300

    calculadora

--- script.js
+++ script.js
@@ -1,52 +1,43 @@
 function calcular() {
-    const peso = document.getElementById('peso').value;
-    const altura = document.getElementById('altura').value;
-    const imc = parseFloat(peso.replace(',', '.')) / Math.pow(parseFloat(altura.replace(',', '.')),2);
-    const res = document.querySelector('.resposta');
-    const p1 = document.createElement('p');
-    const p2 = document.createElement('p');
-    
-    if (peso <= 0 || altura <= 0 || peso.length == 0 || altura.length == 0) {
-        window.alert('Error: Insira peso e altura maior que zero!')
-    } else if (imc < 18.5) {
-        p1.innerHTML = `Seu imc foi de ${imc.toFixed(1)}`
-        p2.innerHTML = `Você está muito abaixo do peso ideal!`
-        p1.setAttribute('class', 'resultado');
-        p2.setAttribute('class', 'resultado');
-        res.appendChild(p1);
-        res.appendChild(p2);
+    const displayValor = document.getElementById('resultado-valor')
+    const displayResultado = document.getElementById('resultado')
+    const peso = document.getElementById('peso').value
+    const altura = document.getElementById('altura').value
+    const pesoValor = parseFloat(peso.replace(',', '.'))
+    const alturaValor = parseFloat(altura.replace(',', '.'))
+
+    const imc = pesoValor / Math.pow(alturaValor, 2)
+
+    if (peso <= 0 && altura <= 0) {
+        window.alert('Error: Peso e altura deve ser maior que zero.')
+        e.preventDefault()
+    }
+
+    if (imc <= 18.5) {
+        displayValor.innerText = `Seu calculo do IMC foi de: ${imc.toFixed(2)}`
+        displayResultado.innerText = `Você está abaixo do peso normal!`
     } else if (imc >= 18.5 && imc <= 24.9) {
-        p1.innerHTML = `Seu imc foi de ${imc.toFixed(1)}`
-        p2.innerHTML = `Você está no seu peso ideal!`
-        p1.setAttribute('class', 'resultado');
-        p2.setAttribute('class', 'resultado');
-        res.appendChild(p1);
-        res.appendChild(p2);
-    } else if (imc >= 25 && imc <= 29.9) {
-        p1.innerHTML = `Seu imc foi de ${imc.toFixed(1)}`
-        p2.innerHTML = `Você está com obesidade de grau 1!`
-        p1.setAttribute('class', 'resultado');
-        p2.setAttribute('class', 'resultado');
-        res.appendChild(p1);
-        res.appendChild(p2);
-    } else if (imc >= 30 && imc <= 39.9) {
-        p1.innerHTML = `Seu imc foi de ${imc.toFixed(1)}`
-        p2.innerHTML = `Você está com obesidade de grau 2!`
-        p1.setAttribute('class', 'resultado');
-        p2.setAttribute('class', 'resultado');
-        res.appendChild(p1);
-        res.appendChild(p2);
+        displayValor.innerText = `Seu calculo do IMC foi de: ${imc.toFixed(2)}`
+        displayResultado.innerText = `Você está com o peso normal!`
+    } else if (imc >= 25 && imc <= 29) {
+        displayValor.innerText = `Seu calculo do IMC foi de: ${imc.toFixed(2)}`
+        displayResultado.innerText = `Você está com Excesso de peso!`
+    } else if (imc >= 30 && imc <= 34.9) {
+        displayValor.innerText = `Seu calculo do IMC foi de: ${imc.toFixed(2)}`
+        displayResultado.innerText = `Você está com obesidade de grau 1!`
+    } else if (imc >= 35 && imc <= 39.9){
+        displayValor.innerText = `Seu calculo do IMC foi de: ${imc.toFixed(2)}`
+        displayResultado.innerText = `Você está com obesidade de grau 2!`
     } else {
-        p1.innerHTML = `Seu imc foi de ${imc.toFixed(1)}`
-        p2.innerHTML = `Você está com obesidade de grau 3!`
-        p1.setAttribute('class','resultado');
-        p2.setAttribute('class','resultado');
-        res.appendChild(p1);
-        res.appendChild(p2);
+        displayValor.innerText = `Seu calculo do IMC foi de: ${imc.toFixed(2)}`
+        displayResultado.innerText = `Você está com obesidade de grau 3!`
     }
 }
 
-function limparCalculo() {
-    const res = document.querySelector('.resposta');
-    res.innerHTML = '';
+function limpar () {
+    const displayValor = document.getElementById('resultado-valor')
+    const displayResultado = document.getElementById('resultado')
+
+    displayResultado.innerText = ''
+    displayValor.innerText = ''
 }
